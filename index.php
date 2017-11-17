d49c6709bea5bcf095c191f0e4562082b5294f5a
Paulo Franco <paulimsantos@gmail.com>
2017-11-17 17:51:08 -0200

    Cant remember what changed

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
  	<title>Relfinder</title>
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.css">
 </head>
 <body>
   <nav class="navbar is-transparent" id="top">
