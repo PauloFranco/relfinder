1d9b1e2e8a4c88b460495c28a76db4e8ca08d0b0
PauloFranco <paulimsantos@gmail.com>
2018-03-05 11:45:00 -0300

    Started lda

--- lda.py
+++ lda.py
@@ -1,25 +1,83 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from matplotlib import pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import cross_validation
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from matplotlib import colors
-# %% Read data from csv file
-A = np.loadtxt('dataset.txt', delimiter=',')
-#Get the targets (first column of file)
-y = A[:, 0]
-#Remove targets from input data
-A = A[:, 1:]
+import math
+from sklearn.preprocessing import LabelEncoder
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-lda = LinearDiscriminantAnalysis(n_components=2)
-lda.fit(A, y)
-drA = lda.transform(A)
+feature_dict = {i:label for i,label in zip(
+                range(2),
+                  ('BUSAGE',
+                  'DAYSDLQ',
+                   ))}
 
-# %% Data extracted; perform LDA
-lda = LinearDiscriminantAnalysis()
-k_fold = cross_validation.KFold(len(A), 3, shuffle=True)
-print('LDA Results: ')
-for (trn, tst) in k_fold:
-    lda.fit(A[trn], y[trn])
-    outVal = lda.score(A[tst], y[tst])
-    #Compute classification error
-print('Score: ' + str(outVal))
+
+df = pd.io.parsers.read_csv(
+    filepath_or_buffer='dataset.txt',
+    header=None,
+    sep=',',
+    )
+
+
+df.columns = [l for i,l in sorted(feature_dict.items())] + ['class label']
+df.dropna(how="all", inplace=True) # to drop the empty line at file-end
+df.tail()
+
+
+X = df[['BUSAGE','DAYSDLQ']].values
+y = df['class label'].values
+
+enc = LabelEncoder()
+label_encoder = enc.fit(y)
+
+
+
+y = label_encoder.transform(y) + 1
+
+label_dict = {1: 'Negativo', 2: 'Positivo'}
+
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12,6))
+
+for ax,cnt in zip(axes.ravel(), range(2)):  
+
+    # set bin sizes
+    min_b = math.floor(np.min(X[:,cnt]))
+    max_b = math.ceil(np.max(X[:,cnt]))
+    bins = np.linspace(min_b, max_b, 25)
+
+    # plottling the histograms
+    for lab,col in zip(range(1,3), ('blue', 'red')):
+        ax.hist(X[y==lab, cnt],
+                   color=col,
+                   label='class %s' %label_dict[lab],
+                   bins=bins,
+                   alpha=0.5,)
+    ylims = ax.get_ylim()
+
+    # plot annotation
+    leg = ax.legend(loc='upper right', fancybox=True, fontsize=8)
+    leg.get_frame().set_alpha(0.5)
+    ax.set_ylim([0, max(ylims)+2])
+    ax.set_xlabel(feature_dict[cnt])
+    ax.set_title('Histogram #%s' %str(cnt+1))
+
+    # hide axis ticks
+    ax.tick_params(axis="both", which="both", bottom="off", top="off",  
+            labelbottom="on", left="off", right="off", labelleft="on")
+
+    # remove axis spines
+    ax.spines["top"].set_visible(False)  
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)    
+
+axes[0][0].set_ylabel('count')
+axes[1][0].set_ylabel('count')
+
+fig.tight_layout()       
+
+plt.show()
+
+np.set_printoptions(precision=4)
